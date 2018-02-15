e52cb966f0a14906cc3ede4b5407a1703e3299bd
aalfahad <boomar14@hotmail.com>
2018-02-15 16:37:39 +0300

    foodhub project from yahya with base file

---
+++ restaurants/views.py
@@ -0,0 +1,84 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Restaurant
+from .forms import RestaurantForm
+
+def list(request):
+	context = {
+	"restaurants": Restaurant.objects.all(),
+
+	}
+	return render(request, 'restaurant_list.html', context)
+
+
+	# calls the html file detailview.html
+
+
+def detail(request, restaurant_id):
+	context = {
+	"restaurant_detail": Restaurant.objects.get(id=restaurant_id),
+
+	}
+
+
+	return render(request, 'restaurant_detail.html', context)
+
+def create(request):
+	form = RestaurantForm()
+	if request.method == "POST":
+		form = RestaurantForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect("restaurant_list")
+	context = {
+		"create_form" : form,
+	}
+	return render(request,'restaurant_create.html',context)
+
+def update(request, restaurant_id):
+	restaurant_obj = Restaurant.objects.get(id=restaurant_id)
+	form = RestaurantForm(instance = restaurant_obj)
+	if request.method == "POST":
+		form = RestaurantForm(request.POST, instance=restaurant_obj)
+		if form.is_valid():
+			form.save()
+			return redirect("restaurant_detail",restaurant_id=restaurant_obj.id)
+	context = {
+		"restaurant_obj": restaurant_obj,
+		"create_form" : form,
+	}
+	return render(request,'restaurant_update.html',context)
+
+def delete(request, restaurant_id):
+	Restaurant.objects.get(id=restaurant_id).delete()
+	return redirect("restaurant_list")
+
+
+		# meals_dictionary = {
+	# 	"meals": [
+	# 	  {
+	# 		 "title": "Swiss Cheese Burger",
+	# 		 "content": "Description, ingredients, sauces, vegetables, calories",
+	# 		 "created": "2018-02-05",
+	# 		 "updated": "2018-02-06",
+	# 	   },
+	# 	   {
+	# 		 "title": "Classic Cheese Burger",
+	# 		 "content": "Description, ingredients, sauces, vegetables, calories",
+	# 		 "created": "2018-02-02",
+	# 		 "updated": "2018-02-04",
+	# 	   },
+	# 	   {
+	# 		 "title": "Barbecue Burger",
+	# 		 "content": "Description, ingredients, sauces, vegetables, calories",
+	# 		 "created": "2018-02-06",
+	# 		 "updated": "2018-02-09",
+	# 	   },
+	# 	   {
+	# 		 "title": "Special Burger",
+	# 		 "content": "Description, ingredients, sauces, vegetables, calories",
+	# 		 "created": "2018-02-01",
+	# 		 "updated": "2018-02-01",
+	# 	   }
+	# ]
+	# }
